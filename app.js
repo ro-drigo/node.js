22bd81be407e4c9082dd76d679bccf1fd6600b2a
ro-drigo <rodrigo.santos7202@gmail.com>
2020-05-16 17:15:35 -0300

    Inserting on MySQL DB with Node.Js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 //Conexão com BD MySQL
 const mysql = require("mysql");
 
+//A partir do MySQL 8 apresenta erro ao utilizar o usuário root para conexão
+//Necessário criar novo usuário
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'rodrigoferreira',
@@ -19,37 +21,23 @@ connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
       return;
+    }else{
+        console.log('conectado');
     }
-   
-    console.log('connected as id ' + connection.threadId);
   });
 
-//Select feito para testar a conexão
-connection.query('SELECT * FROM users', function(err, rows, fields){
-    if(!err){
-        console.log('Resultado: ', rows);
-    }else{
-        console.log('Erro ao realizar a consulta.');
-    }
-});
+//Inserindo dados na tabela
+connection.query("INSERT INTO users(nome, email) VALUES ('Paulo','paulo@hotmail.com')", function(err, result){
+                        if(!err){
+                            console.log("Usuario cadastrado com sucesso");
+                        }else{
+                            console.log("Erro ao cadastrar");
+                        }
+  });
 
-//Indicar página que quer abrir (o __dirname é necessário para pegar o caminho absoluto)
-app.get("/", function(req, res){
-    //send nos dá uma mensagem
-    res.sendFile(__dirname + "/src/index.html");
-});
 
-app.get("/blog", function(req, res){
-    res.send("Página do blog");
-});
 
-app.get("/sobre-empresa", function(req, res){
-    res.sendFile(__dirname + "/src/sobre-empresa.html");
-});
 
-app.get("/contato", function(req, res){
-    res.send("Página de contato");
-});
 
 //Iniciando servidor
 app.listen(8080);
