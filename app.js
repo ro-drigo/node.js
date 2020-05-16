54b8051960a78f80ef3ccd982f52e9ef24a13fe5
ro-drigo <rodrigo.santos7202@gmail.com>
2020-05-16 16:35:40 -0300

    Adding connection with MySQL DB and testing

--- app.js
+++ app.js
@@ -4,6 +4,35 @@ const express = require("express");
 //Instanciando função do express
 const app = express();
 
+//Conexão com BD MySQL
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'rodrigoferreira',
+    password: '123456',
+    database: 'celke'
+});
+
+//Verificando se a conexão está funcionando
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+//Select feito para testar a conexão
+connection.query('SELECT * FROM users', function(err, rows, fields){
+    if(!err){
+        console.log('Resultado: ', rows);
+    }else{
+        console.log('Erro ao realizar a consulta.');
+    }
+});
+
 //Indicar página que quer abrir (o __dirname é necessário para pegar o caminho absoluto)
 app.get("/", function(req, res){
     //send nos dá uma mensagem
